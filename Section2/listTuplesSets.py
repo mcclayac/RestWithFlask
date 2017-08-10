f106bf009a687eb28220ba49705181f4f756a4ef
amcclay <anthony.mcclay@me.com>
2017-08-10 03:33:56 -0400

    Initial commit

---
+++ Section2/listTuplesSets.py
@@ -0,0 +1,55 @@
+
+
+
+
+my_variable = 99
+
+grades_list = [77, 88, 90, 100, 95]  # Ordered List
+grades_tuple = (77, 88, 90, 100, 96)  # immutable
+grades_set = {77, 80, 90, 95, 100, 100} # unique and uordered
+
+grades_list.append(87)
+grades_list.append(55)
+
+# print(sum(grades_list) / len(grades_list))
+#
+# print(sum(grades_tuple) / len(grades_tuple))
+#
+# print(sum(grades_set) / len(grades_set))
+# print(grades_set)
+#
+# grades_tuple = grades_tuple + (55,)
+# print(grades_tuple)
+#
+# grades_list[0]= 60
+# print(grades_list[0])
+# print(grades_list)
+#
+# print(grades_tuple)
+#
+# grades_set.add(44)
+# print(grades_set)
+
+########  Set operations
+
+# set_one = {1, 2, 3 , 4 , 5}
+# set_two = {1, 3, 5, 7, 9, 11}
+
+your_lottery_numbers = {1, 2, 3 , 4 , 5}
+winning_numbers = {1, 3, 5, 7, 9, 11}
+
+# print(your_lottery_numbers.intersection(winning_numbers))
+#print(your_lottery_numbers.union(winning_numbers))
+#print({1,2,3,4}.difference({1,2}))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Section2/variablesMethods.py
@@ -0,0 +1,34 @@
+
+
+a = 5
+b= 10
+my_variable = 56
+my_variable_name = 10
+
+string_variable = "helllo"
+string_single_qoutes = 'string can have single qoutes'
+
+# print(my_variable)
+# print(string_variable)
+# print('Hello, World these nuts')
+# print(123)
+
+
+##
+
+def my_print_method(my_arguement):
+    # print('hello')
+    print(my_arguement)
+
+
+# my_print_method('Tony')
+
+def my_multiply_method(number_one, number_two):
+    return number_one * number_two
+
+
+# result = my_multiply_method(5, 3)
+# my_print_method(result)
+my_print_method(my_multiply_method(5, 3))
+# print(result)
+
